a51f1ea665ac810b2a08f84fafa4131bc065c41f
Simeoncoded <simeonolawore07@gmail.com>
2025-01-05 14:12:47 -0500

    first commit

---
+++ DataStructures practice/DataStructures practice/Program.cs
@@ -0,0 +1,20 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+
+    static void Main()
+    {
+
+        // printing alternate values
+        int[] arr = { 10, 20, 30, 40, 50 };
+
+        for (int i = 0; i < arr.Length; i += 2)
+        {
+            Console.WriteLine(arr[i] + " ");
+         
+        }
+    }
+}
